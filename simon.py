314b720407fa45db5320affda6de283bd8f12fdf
marcuskamryn <2025marcus.kamryn@fcboe.org>
2024-10-01 13:46:12 -0400

    Update simon.py
    DocStrings added to the functions

--- simon.py
+++ simon.py
@@ -31,11 +31,20 @@ white_button.direction = Direction.INPUT
 def add_to_sequence(lst):
     lst.append(random.randint(0, 3))
     
+    """
+Adds a random number between 0-4 to the list
+"""
+    
 def on_off(name):
     name.value = True
     time.sleep(0.3)
     name.value = False
     time.sleep(0.3)
+    
+"""
+Makes the LED light blink
+
+"""
 
 def display_sequence(lst):
     for x in lst:
@@ -47,6 +56,10 @@ def display_sequence(lst):
             on_off(yellow)
         if x == 3:
             on_off(blue)
+            
+"""
+For the number of items in the list it turns on the light that corresponds with the number in lst
+"""
 
 def user_sequence(lst):
     if blue_button.value:
@@ -57,14 +70,29 @@ def user_sequence(lst):
          lst.append(1)
     if red_button.value:
         lst.append(0)
+    
+"""
+Adds the values in the list based of the user's input. Depending on what button that is pressed it adds that number to the list.
+
+"""
 
 def reset():
     game_start = False
+    print(game_start)
     the_list = []
+    print(the_list)
     user_list = []
     display_score()
+    print(score)
+    
     """
-resets the game after 
+Sets game_start to false
+
+Set's simions list to empty
+
+Set's user list to empty
+
+Display's the user score
     """
     
 def display_score():
@@ -76,19 +104,31 @@ def display_score():
         on_off(red)
     
     """
-Supposed to display the final score with the first leds on the right hand side of my board bu using the green light as the tens
+Supposed to display the final score with the first leds on the right hand side of my board by using the green light as the tens
 place and the red light as the ones place.
     """
         
 def check_list(lst_one, lst_two):
     global score
     
+    for i in range(len(lst_one)):
+        if lst_one[i] == lst_two[i]:
+            score = score + 1
+            print("yes")
+        else:
+            reset()
+            print("no")
+"""
+Has the score as a global varible so it'll change outside of the function
+
+Goes through a loop for how evere many number of values is in the first list
+
+    if the value of the first list matches the value of the second list
+        then it'll update the score by 1
+    if the values of those list doesnt match up then it calls the function reset()
+
+"""
     
-    
-    if lst_one == lst_two:
-        score = score + 1
-    else:
-        reset()
         
 the_list = []
 user_list = []
@@ -98,9 +138,13 @@ score = 0
 while True:
     if white_button.value:
         add_to_sequence(the_list)
+        print(the_list)
         display_sequence(the_list)
-        pass
+        #pass
     if blue_button.value or green_button.value or yellow_button.value or red_button.value:
         user_sequence(user_list)
+        print(user_list)
         display_sequence(user_list)
+        time.sleep(0.2)
+        check_list(the_list, user_list)
         pass
